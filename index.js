cb4d7b0d613a03f6f386d393e18e88ae84b8dad8
Ewan Forbes <ewan_forbes@yahoo.co.uk>
2018-01-04 13:47:01 +0000

    Added displaying all stops on each route

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const request = require("request");
-let data;
+const http = require("http");
+const port = 80;
+let tubeData;
+let dlrData;
+let victoriaData;
+let routes = [];
 
-async function programLoop() {
-	await getData("https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=deddaca2&app_key=43f241fe9e184b6aa7de7b615b28a6cb");
-	data = JSON.parse(data);
-	for (var key in data) {
-		if (data.hasOwnProperty(key)) {
-			console.log(data[key].name + " " + JSON.stringify(data[key].lineStatuses[0].statusSeverityDescription));
+async function getAllData() {
+	tubeData = await getData("https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=deddaca2&app_key=43f241fe9e184b6aa7de7b615b28a6cb");
+	tubeData = JSON.parse(tubeData);
+	console.log("Got tube data");
+	dlrData = await getData("https://api.tfl.gov.uk/Line/Mode/dlr/Status?detail=true&app_id=deddaca2&app_key=43f241fe9e184b6aa7de7b615b28a6cb");
+	dlrData = JSON.parse(dlrData);
+	console.log("Got DLR data");
+	victoriaData = await getData("https://api.tfl.gov.uk/Line/victoria/Route/Sequence/inbound?serviceTypes=Regular&excludeCrowding=true&app_key=43f241fe9e184b6aa7de7b615b28a6cb&app_id=deddaca2");
+	victoriaData = JSON.parse(victoriaData);
+	routes = [];
+	for (var key in tubeData) {
+		let data = await getData("https://api.tfl.gov.uk/Line/" + tubeData[key].id + "/Route/Sequence/inbound?serviceTypes=Regular&excludeCrowding=true&app_key=43f241fe9e184b6aa7de7b615b28a6cb&app_id=deddaca2");
+		data = JSON.parse(data);
+		routes.push([]);
+		for (var val in data.stopPointSequences[0].stopPoint) {
+			routes[key].push(JSON.stringify(data.stopPointSequences[0].stopPoint[val].name));
 		}
 	}
+	console.log(routes);
 }
 
 function getData(site) {
@@ -17,9 +33,29 @@ function getData(site) {
 			uri: site,
 		}, function(error, response, body) {
 			data = body;
-			resolve("Success!");
+			resolve(data);
 		});
 	});
 }
 
-programLoop();
+
+getAllData();
+let timeout = setInterval(getAllData, 60000);
+
+http.createServer(function (req, res) {
+	console.log("Request recieved");
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("<h1>Tube Line Status</h1>")
+	for (var key in tubeData) {
+		if (tubeData.hasOwnProperty(key)) {
+			res.write("<h2>" + tubeData[key].name + ": </h2>" + JSON.stringify(tubeData[key].lineStatuses[0].statusSeverityDescription));
+			res.write("<br>" + routes[key].toString());
+		}
+	}
+	res.write("<h1>DLR Line Status</h1>");
+	res.write(JSON.stringify(dlrData[0].lineStatuses[0].statusSeverityDescription));
+	res.end();
+	console.log("Reply sent");
+}).listen(port);
+
+console.log("Listening on port: " + port);
