ec372859a4380826bf31a1f786bc28055ead6663
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2018-01-25 09:59:35 +0000

    Added bootstrap navbar

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(document).ready(() => {
 	$(".route-div").hide();
 
 	$(".route-toggler").on("click", function() {
-		$(this).nextAll(".route-div").toggle();
+		$(this).nextAll(".route-div").toggle("slow");
 	});
 });
